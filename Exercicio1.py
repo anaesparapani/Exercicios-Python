af3e8bf0706eb686b95dd17ca29ed155eba2bd0b
anaesparapani <159201364+anaesparapani@users.noreply.github.com>
2024-03-11 18:18:41 -0300

    Add files via upload

---
+++ Exercicio1.py
@@ -0,0 +1,3 @@
+sal = float(input("Digite o salário: ")) #LEIA salario
+novosal = sal + sal * 25 / 100 #novosal = sal + sal * 25/100
+print("Novo salário: ", novosal) #ESCREVA novosal
---
+++ Exercicio2.py
@@ -0,0 +1,6 @@
+sal = float(input("Digite o salário: "))
+perc = float(input("Digite o percentual: "))
+aumento = sal * perc / 100
+print("O aumento é: ", aumento)
+novosal = sal + aumento
+print("Novo salário: ", novosal)
